1ad98ec18a5bb948429eb967ae33e7e02d1dd5e3
elnikov <elnikov.s@gmail.com>
2023-03-25 18:01:51 +0300

    first commit

---
+++ connection.go
@@ -0,0 +1,29 @@
+package connection
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+var DB *sql.DB
+
+func ConnectDb() {
+	fmt.Println("Go MySQL Tutorial")
+	var err error
+	DB, err = sql.Open("mysql", "main:qwerty@tcp(127.0.0.1:3306)/highload")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Connected")
+	migration()
+
+	defer DB.Close()
+}
+
+func migration() {
+	createPageQuery, err := DB.Query("CREATE TABLE IF NOT EXISTS pages (slug varchar(2body text)")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer createPageQuery.Close()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module highload
+
+go 1.16
+
+require github.com/go-sql-driver/mysql v1.7.0 // indirect
